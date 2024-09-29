d2933a974fc75df72f8f671222cab3049e472229
annisa aulia <auliaannisa146@gmail.com>
2024-09-29 22:25:14 +0700

    menambahkan halaman edit buku tamu dan fitur ubah data tamu

--- buku_tamu.php
+++ buku_tamu.php
@@ -1,6 +1,6 @@
 <?php
 include_once('templates/header.php');
-include_once('function.php')
+include_once('function.php');
 
 ?>
 
@@ -14,8 +14,8 @@ include_once('function.php')
 
     <?php
     // jika ada tombol simpan
-    if (isset ($_POST['simpan'])) {
-        if (tambah_tamu(($_POST) > 0)) {
+    if (isset($_POST['simpan'])) {
+        if (tambah_tamu($_POST) > 0) {
 
     ?>  
             <div class="alert alert-success" role="alert">
@@ -24,7 +24,7 @@ include_once('function.php')
         <?php
         } else {
         ?>
-            <div class="alert alert-danger">
+            <div class="alert alert-danger" role="alert">
                 Data gagal disimpan!
             </div>
     <?php
@@ -34,69 +34,60 @@ include_once('function.php')
      
 
    
-</div>
 
-<!-- DataTales Example -->
-<div class="card shadow mb-4">
-    <div class="card-header py-3">
-        <button type="button" class="btn btn-primary btn-icon-split" data-toggle="modal" data-target="#tambahModal">
-            <span class="icon text-white-50">
-                <i class="fas fa-plus"></i>
-            </span>
-            <span class="text">Data Tamu</span>
-        </button>
-    </div>
-    <div class="card-body">
-        <div class="table-responsive">
-            <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Position</th>
-                        <th>Office</th>
-                        <th>Age</th>
-                        <th>Start date</th>
-                        <th>Salary</th>
-                    </tr>
-                </thead>
-                <tfoot>
-                    <tr>
-                        <th>Name</th>
-                        <th>Position</th>
-                        <th>Office</th>
-                        <th>Age</th>
-                        <th>Start date</th>
-                        <th>Salary</th>
-                    </tr>
-                </tfoot>
-                <tbody>
-                    <?php
-                    // penomoran auto-increment
-                    $no = 1;
-                    // query untuk memanggil semua data  dari tabel buku_tamu
-                    $buku_tamu =  query("SELECT * FROM  buku_tamu");
-                    foreach ($buku_tamu as $tamu) :
-                    ?>
+
+    <!-- DataTales Example -->
+    <div class="card shadow mb-4">
+        <div class="card-header py-3">
+            <button type="button" class="btn btn-primary btn-icon-split" data-toggle="modal" data-target="#tambahModal">
+                <span class="icon text-white-50">
+                    <i class="fas fa-plus"></i>
+                </span>
+                <span class="text">Data Tamu</span>
+            </button>
+        </div>
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                    <thead>
                         <tr>
-                            <td><?= $no++; ?> </td>
-                            <td><?= $tamu['tangga'] ?> </td>
-                            <td><?= $tamu['nama_tamu'] ?> </td>
-                            <td><?= $tamu['alamat'] ?> </td>
-                            <td><?= $tamu['no_hp'] ?> </td>
-                            <td><?= $tamu['bertemu'] ?> </td>
-                            <td><?= $tamu['kepentingan'] ?> </td>
+                            <th>No</th>
+                            <th>Tanggal</th>
+                            <th>Nama Tamu</th>
+                            <th>Alamat</th>
+                            <th>No. Telp/HP</th>
+                            <th>Bertemu dg.</th>
+                            <th>Kepentingan</th>
+                            <th>Aksi</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        // penomoran auto-increment
+                        $no = 1;
+                        // query untuk memanggil semua data  dari tabel buku_tamu
+                        $buku_tamu =  query("SELECT * FROM  buku_tamu");
+                        foreach ($buku_tamu as $tamu) : ?>
+                        <tr>
+                            <td><?= $no++; ?></td>
+                            <td><?= $tamu['tanggal'] ?></td>
+                            <td><?= $tamu['nama_tamu'] ?></td>
+                            <td><?= $tamu['alamat'] ?></td>
+                            <td><?= $tamu['no_hp'] ?></td>
+                            <td><?= $tamu['bertemu'] ?></td>
+                            <td><?= $tamu['kepentingan'] ?></td>
                             <td>
-                                <button class="btn btn-success" type="button">Ubah</button>
+                                <a href="edit-tamu.php?id=<?= $tamu['id_tamu']?>" class="btn btn-success">Ubah</a>
                                 <button class="btn btn-danger" type="button">Hapus</button>
                             </td>
                         </tr>
-                    <?php endforeach; ?>
-                </tbody>
-            </table>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
 </div>
-
 <?php
 // mengambil data barang dari tabel dengan kode terbesar
 $query = mysqli_query($koneksi, "SELECT max(id_tamu) as kodeTerbesar FROM buku_tamu");
@@ -114,15 +105,15 @@ $urutan++;
 
 // angka yang diambil tadi digabungkan dengan kode huruf yang kita inginkan, misalnya zt
 $huruf = "zt";
-$kodeTamu = $huruf . sprintf("%03s", $urutan);
+$kodeTamu = $huruf . sprintf("%02s", $urutan);
 ?>
 
 <!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade" id="tambahModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                <h5 class="modal-title" id="tambahModalLabel">Modal title</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
@@ -139,7 +130,7 @@ $kodeTamu = $huruf . sprintf("%03s", $urutan);
                     <div class="form-grup row">
                         <label for="alamat" class="col-sm-3 col-form-label">Alamat</label>
                         <div class="col-sm-8">
-                            <input type="text" class="form-control" id="alamat" name="alamat">
+                            <textarea name="alamat" id="alamat" class="form-control"></textarea>
                         </div>
                     </div>
                     <div class="form-grup row">
@@ -160,12 +151,13 @@ $kodeTamu = $huruf . sprintf("%03s", $urutan);
                             <input type="text" class="form-control" id="kepentingan" name="kepentingan">
                         </div>
                     </div>
+                
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="submit" name="simpan" class="btn btn-primary">simpan</button>
+                    </div>
                 </form>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                <button type="submit" class="btn btn-primary">Save changes</button>
-            </div>
         </div>
     </div>
 </div>
--- index.php
+++ index.php
@@ -1,11 +1,3 @@
-                <!-- Begin Page Content -->
-                <div class="container-fluid">
-
-                    <!-- Page Heading -->
-                    <h1 class="h3 mb-4 text-gray-800">Blank Page</h1>
-
-                </div>
-                <!-- /.container-fluid -->
 
 <?php
 include_once('templates/header.php');
